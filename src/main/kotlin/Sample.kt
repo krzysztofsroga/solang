ff32ae38369a2f85d0c8c523f8470c50a2f0082b
Krzysztof Sroga <krzysztof@sroga.pw>
2019-11-17 17:17:21 +0100

    extracted stacksort code to method

--- src/main/kotlin/Sample.kt
+++ src/main/kotlin/Sample.kt
@@ -3,26 +3,23 @@ import kotlinx.serialization.UnstableDefault
 
 @UnstableDefault
 fun main() = createCode {
-    runBlocking {
-//        StackOverflowConnection.getAcceptedAnswers(listOf("sorting", "python"), 3).forEach { println(it) }
-//        val fetchedAnswers = StackOverflowConnection.getAcceptedAnswers(listOf("sorting", "python"), 10)
-//        val a = fetchedAnswers.first().getRawData()
-
-
-
-        val fetchedAnswers = SearchSnippet(listOf("sorting", "python"), 10)
-        fetchedAnswers.forEachIndexed { i, fetchedAnswer ->
-            +SimpleSnippet("\n\n\n\nDownloaded Snipped number $i:\n")
-            +fetchedAnswer
-        }
-
-//        println("Resulting answers: ${fetchedAnswers.size}")
-    }
     SoLangConfiguration.soLangMode = SoLangConfiguration.SoLangMode.PRINT
     SoLangConfiguration.apiToken = "" //YOUR TOKEN GOES HERE
+
+    stackSort()
 
 //    +StackOverflowSnippet(answerNumber = 50165755) toLine 15 //declare randomdate
 //    +StackOverflowSnippet(answerNumber = 48980683, codeBlockNumber = 2) toLine 1 change ("random.random" to "randomTime") change ("x" to "my_list")  //generate list
 //    +StackOverflowSnippet(answerNumber = 902736, codeBlockNumber = 6) fromLine 3 change ("&gt;" to ">")//declare bubble and print sorted lits
 }.buildWith("python2", "script.py")
+
+
+@UnstableDefault
+fun CodeBuilder.stackSort() = runBlocking{
+    val fetchedAnswers = SearchSnippet(listOf("sorting", "python"), 10)
+    fetchedAnswers.forEachIndexed { i, fetchedAnswer ->
+        +SimpleSnippet("\n\n\n\nDownloaded Snipped number $i:\n")
+        +fetchedAnswer
+    }
+}
 
